5d3c326056c388e2c7394deaf05e1591588abd97
Chris Cardle <cdcardle@gmail.com>
2018-12-19 15:58:57 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,11 @@ class ProductsController < ApplicationController
   end
 
   def create
-
+    Product.create(product_params)
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 end
